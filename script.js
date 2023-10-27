9968c973f195f1f253880c7810ddba9872291586
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2023-10-27 20:42:39 +0400

    Add a function to refresh display after user input
    Values of "first/second operand" and "operation" now
    placed inside calculator object as an array

--- script.js
+++ script.js
@@ -5,20 +5,31 @@ const calculator = {
         "*": (a, b) => +a * +b,
         "/": (a, b) => +a / +b,
     },
+    // first/second operand and some operation
+    expression: ["0", "0", ""],
 };
 
-let firstOperand = "0";
-let secondOperand = "0";
-let operation = "";
+function addNewNumber(indexOfOperand, newSymbol) {
+    // if there's a leading zero - override
+    if (calculator.expression[indexOfOperand][0] === "0")
+        calculator.expression[indexOfOperand] = newSymbol;
+    else calculator.expression[indexOfOperand] += newSymbol;
+    currNumberDisplay.textContent = calculator.expression[indexOfOperand];
+}
+
 
 const buttons = document.querySelectorAll(".forInput");
 let btn = document.querySelector(".decimal");
+let currNumberDisplay = document.querySelector(".currNumber");
+let prevExprDisplay = document.querySelector(".prevExpression");
 
 for (let btn of Array.from(buttons)) {
     btn.addEventListener('click', () => {
-        if (operation === "")
-            firstOperand += btn.textContent;
-        else secondOperand += btn.textContent;
+        /*  if user choose some operation, then we're dealing
+            with second operand  */
+        if (!calculator.expression[2])
+            addNewNumber(0, btn.textContent);
+        else addNewNumber(1, btn.textContent);
     });
 }
 
@@ -27,3 +38,7 @@ btn.addEventListener('click', () => {
     btn.style.backgroundColor = "lightskyblue";
     btn.style.borderColor = "dimgrey";
 });
+
+/* currNumberDisplay.addEventListener(''() => {
+
+}); */
