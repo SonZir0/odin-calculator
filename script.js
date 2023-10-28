38689b4d6160a85e0e00eabeefc2c08453aeb3ec
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2023-10-28 21:54:33 +0400

    Overdue commit, more info in full message
    Add three things:
    1)  Change to add leading zeros to empty operands if user clicks decimal
        point. Cosmetic mostly, but consistency is good
    
    2)  Fixed addition of newly choosen operation symbols to dysplay instead
        of replacing the old ones. Display could've looked like:
        "operand1 + + + / * - +" but only the last one mattered. Now new one
        replaces the old one.
    
    3)  Futher work on decimal button. Now button activates/disables on
        transition between first and second operands through a check if that
        number had a decimal point or not (info is stored in calculator
        object and refreshes on calculation steps too) in addition to old
        checks for addition/deletion of decimal point through input buttons.

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ const calculator = {
         "*": (a, b) => +a * +b,
         "/": (a, b) => +a / +b,
     },
-    // first/second operand and some operation
-    expression: ["0", "", ""],
+
+    expression: ["0", "", ""],  //  first/second operand and some operation
+    isDecimal: [false, false],  //  operand contains decimal point
 
     eval() {
         showPastExpression();
         currNumberDisplay.textContent =
             this.calculate[this.expression[2]](this.expression[0], this.expression[1]);
         this.expression = [`${currNumberDisplay.textContent}`, "", ""];
+        this.isDecimal[1] = false;
+        this.isDecimal[0] = !Number.isInteger(+currNumberDisplay.textContent);
+        if (this.isDecimal[0])
+            decimalBtn.disabled = true;
+        else decimalBtn.disabled = false;
     },
 }
 
@@ -37,35 +43,43 @@ function get1OperandAndOperation() {
 }
 
 function addNewNumber(indexOfOperand, newSymbol) {
-    // if there's a leading zero - override
-    if (calculator.expression[indexOfOperand][0] === "0" &&
-        calculator.expression[indexOfOperand].length === 1 && newSymbol != '.')
+    if (newSymbol === ".")
+        calculator.isDecimal[indexOfOperand] = true;
+    /*  if there's a leading zero - override with anything but decimal dot
+        if it's decimal but there's no leading 0 - add it   */
+    if (newSymbol === "." && calculator.expression[indexOfOperand] === "")
+        calculator.expression[indexOfOperand] = "0.";
+    else if (newSymbol !== "." && calculator.expression[indexOfOperand] === "0")
         calculator.expression[indexOfOperand] = newSymbol;
     else calculator.expression[indexOfOperand] += newSymbol;
+
     currNumberDisplay.textContent = calculator.expression[indexOfOperand];
 }
 
 function clearLastSymbols(indexOfOperand) {
-    if (calculator.expression[indexOfOperand].slice(-1) === ".")
+    if (calculator.expression[indexOfOperand].slice(-1) === ".") {
+        calculator.isDecimal[indexOfOperand] = false;
         decimalBtn.disabled = false;
+    }
+
     calculator.expression[indexOfOperand] = calculator.expression[indexOfOperand].slice(0, -1);
 
     if (calculator.expression[indexOfOperand] === "-")
         calculator.expression[indexOfOperand] = ""; // throw away sign if it's there
-    
+
     else currNumberDisplay.textContent = calculator.expression[indexOfOperand];
 }
 
-let currNumberDisplay = document.querySelector(".currNumber");
-let prevExprDisplay = document.querySelector(".prevExpression");
+const currNumberDisplay = document.querySelector(".currNumber");
+const prevExprDisplay = document.querySelector(".prevExpression");
 const inputButtons = document.querySelectorAll(".forInput");
 const operationBtns = document.querySelectorAll(".operation");
 const displayBtns = document.querySelectorAll(".forDisplay");
-let decimalBtn = document.querySelector(".decimal");
-let evalBtn = document.querySelector(".eval");
-let clearBtn = document.querySelector(".clearAll");
-let clearEntryBtn = document.querySelector(".clearEntry");
-let negativeBtn = document.querySelector(".negative");
+const decimalBtn = document.querySelector(".decimal");
+const evalBtn = document.querySelector(".eval");
+const clearBtn = document.querySelector(".clearAll");
+const clearEntryBtn = document.querySelector(".clearEntry");
+const negativeBtn = document.querySelector(".negative");
 
 for (let btn of Array.from(inputButtons)) {
     btn.addEventListener('click', () => {
@@ -83,9 +97,17 @@ for (let btn of Array.from(inputButtons)) {
 for (let btn of Array.from(operationBtns)) {
     btn.addEventListener('click', () => {
         /*  if there's 2 operand, then operator's choosen too
-            evaluate and line up new operator   */
+            evaluate and line up new operator in the end  */
         if (calculator.expression[1])
             calculator.eval();
+        /*  if there's no second operand - activate decimal button if it was
+            disabled (since we're working with brand new operand), and check
+            if there's another operation symbol on display. If so - replace     */
+        else { 
+            decimalBtn.disabled = calculator.isDecimal[1];
+            if (isNaN(+currNumberDisplay.textContent.slice(-1)))
+                currNumberDisplay.textContent = currNumberDisplay.textContent.slice(0, -2);
+        }
         calculator.expression[2] = btn.textContent;
         currNumberDisplay.textContent += ` ${btn.textContent}`;
     });
@@ -102,6 +124,7 @@ evalBtn.addEventListener('click', () => {
 
 clearBtn.addEventListener('click', () => {
     calculator.expression = ["0", "", ""];
+    calculator.isDecimal = [false, false];
     currNumberDisplay.textContent = "0";
     prevExprDisplay.textContent = "";
     decimalBtn.disabled = false;
@@ -116,10 +139,11 @@ clearEntryBtn.addEventListener('click', () => {
         }
     }
     // if there's a sign choosen - clear top display and refresh main
-    // display to show first operand
+    // display to show first operand. Activate/disable decimal button based on operand
     else if (calculator.expression[2] !== "") {
         calculator.expression[2] = "";
         currNumberDisplay.textContent = calculator.expression[0];
+        decimalBtn.disabled = calculator.isDecimal[0];
     } else {
         clearLastSymbols(0);
         if (calculator.expression[0] === "")    // display decoy zero instead of void
